ebe4eeae3b35cece377d43439a35f2921b11363d
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-14 16:38:50 +0530

    initial mern project setup

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express'); 
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/',(req,res,next) => {
-    res.send("hello world");
-});  
+mongoose.connect("mongodb+srv://himanshubobade:dXlzSE9Q7N2XHG3n@cluster0.g1lf9ck.mongodb.net/code-judge?retryWrites=true&w=majority").then(() =>{
+    app.listen(5000);
+    console.log("database is connected! listening to the localhost 5000");
+}).catch((err) =>console.log(err));
 
-app.listen(5000, () => {
-    console.log("listening to localhost 5000");
-});
