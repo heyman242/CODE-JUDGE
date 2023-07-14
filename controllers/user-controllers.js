b6d21f948fec4ad561330003ab2d8a9030bb421b
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-14 19:05:35 +0530

    jwt functionality for getting user details

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,8 +1,10 @@
 const User = require('../model/User');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET_KEY = "mykey";
 
 
-const signup = async(req, res, next)=>{
+const signup = async (req, res, next)=>{
 
     const {name, email, password} = req.body;
     let existingUser;
@@ -30,7 +32,7 @@ const signup = async(req, res, next)=>{
         console.log(err);
     }
     return res.status(201).json({message:user});
-}
+};
 
 const login = async (req,res,next)=>{
     const{email,password} =req.body;
@@ -43,16 +45,58 @@ const login = async (req,res,next)=>{
     }
 
     if(!existingUser){
-        return res.status(400).json({message:"user not found. Signup please"});
+        return res
+        .status(400)
+        .json({message:"user not found. Signup please"});
     }
     const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
 
     if(!isPasswordCorrect){
-        return res.status(400).json({message:"invaild Email/Password"}); 
+        return res
+        .status(400)
+        .json({message:"invaild Email/Password"}); 
     }
-    return res.status(200).json({message:"Successully Logged In"});
+
+    const token = jwt.sign({id:existingUser._id},JWT_SECRET_KEY,{
+        expiresIn:"1hr"
+    });
+    return res
+    .status(200)
+    .json({message:"Successully Logged In", user:existingUser,token});
+
+};
+
+const verifyToken = (req,res,next) => {
+    const headers = req.headers[`authorization`];
+    const token = headers.split(" ")[1];
+    if(!token){
+        res.status(404).json({message:"no token found"});
+    }
+    jwt.verify(String(token),JWT_SECRET_KEY, (err,user)=>{
+        if(err){
+           return res.status(400).json({message:"unautorized user"});
+        }
+        console.log(user.id);
+        req.id = user.id;
+    });
+    next();
+};
+
+const getUser = async (req,res,next) =>{
+    const userId =  req.id;
+    let user;
+    try{
+        user = await User.findById(userId,"-password");
+    }catch (err){
+        return new Error(err);
+    }
+    if(!user){
+        return res.status(404).json({message:"user not found"});
+    }
+    return res.status(200).json({user})
 
 }
-
 exports.signup = signup;
 exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const { signup, login } = require("../controllers/user-controllers");
+const { signup, login, verifyToken, getUser } = require("../controllers/user-controllers");
 
 const router = express.Router();
 
-router.get('/', (req,res,next) => {
-    res.send("hello world")
-});
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/user', verifyToken, getUser);
+ 
 
 module.exports = router; 
