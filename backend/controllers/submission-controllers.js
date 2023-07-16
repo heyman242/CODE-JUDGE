e5a754b34b6af0b78a1663405a61e8a6c3d789c9
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-16 15:31:15 +0530

    frontend added compile code and langugage option

--- backend/controllers/submission-controllers.js
+++ backend/controllers/submission-controllers.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const { v4: uuid } = require("uuid");
 const { exec } = require("child_process");
 
-
 const baseDir = path.join(__dirname, "..");
 const codesPath = path.join(baseDir, "codes");
 const outputsPath = path.join(baseDir, "outputs");
@@ -27,12 +26,24 @@ const generateFile = async (format, content) => {
 };
 
 const executeCpp = (filepath) => {
-  const jobId = path.basename(filepath).split(".")[0];
-  const outPath = path.join(outputsPath, `${jobId}.out`);
-
   return new Promise((resolve, reject) => {
     exec(
-      `g++ ${filepath} -o ${outPath} && cd ${outputsPath} && ./${jobId}.out`,
+      `g++ ${filepath} -o ${filepath}.out && ${filepath}.out`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject({ error, stderr });
+        } else {
+          resolve(stdout);
+        }
+      }
+    );
+  });
+};
+
+const executePy = (filepath) => {
+  return new Promise((resolve, reject) => {
+    exec(
+      `python3 ${filepath}`,
       (error, stdout, stderr) => {
         if (error) {
           reject({ error, stderr });
@@ -52,12 +63,20 @@ const compileCode = async (req, res) => {
 
   try {
     const filepath = await generateFile(language, code);
-    const output = await executeCpp(filepath);
+    let output = "";
+
+    if (language === "cpp") {
+      output = await executeCpp(filepath);
+    } else if (language === "py") {
+      output = await executePy(filepath);
+    }
 
     return res.status(200).json({ filepath, output });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ success: false, error: "Internal server error" });
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
   }
 };
 
--- frontend/src/components/SolvePage.js
+++ frontend/src/components/SolvePage.js
@@ -1,18 +1,65 @@
-//SolvePage.js
-
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import AceEditor from "react-ace";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 const SolvePage = () => {
-  const [problem, setProblem] = useState(null);
+  const [code, setCode] = useState('');
+  const [language, setLanguage] = useState('cpp');
+  const [output, setOutput] = useState('');
+  const [status, setStatus] = useState('');
+  const [jobId, setJobId] = useState('');
+  const [jobDetails, setJobDetails] = useState(null);
   const { problemId } = useParams();
+  const [problem, setProblem] = useState(null);
+  
 
   useEffect(() => {
+    setCode('');
+    setOutput('');
+    setStatus('');
+    setJobId('');
+    setJobDetails(null);
+    setLanguage('cpp');
+  }, [problemId]);
+
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value);
+  };
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      setOutput('');
+      setStatus('Submitting...');
+      setJobId('');
+
+      const payload = {
+        language,
+        code,
+        problemId,
+      };
+
+      const response = await axios.post('http://localhost:5000/api/solve/:problemId', payload);
+      const { filepath, output: compiledOutput } = response.data;
+
+      setJobId(filepath);
+      setStatus('Submitted');
+      setOutput(compiledOutput);
+    } catch (error) {
+      console.error(error);
+      setStatus('Error');
+      setOutput('An error occurred during code compilation.');
+    }
+  };
+
+   useEffect(() => {
     const fetchProblemDetails = async () => {
       try {
         const res = await axios.get(`http://localhost:5000/api/solve/${problemId}`);
@@ -46,17 +93,34 @@ const SolvePage = () => {
         <h4>Sample Outputs:</h4>
         <p>{sampleOutputs}</p>
         <br/>
-        
+        <div className="output-container">
+          <p>Status: {status}</p>
+          <p>Job ID: {jobId}</p>
+          <p>Output:</p>
+          <pre>{output}</pre>
+        </div>
       </div>
       <div className="code-editor">
+        <div className="language-select">
+          <label htmlFor="language-select">Language:</label>
+          <select id="language-select" value={language} onChange={handleLanguageChange}>
+            <option value="cpp">C++</option>
+            <option value="py">Python</option>
+          </select>
+        </div>
         <AceEditor
-            theme="monokai"
-            fontSize={18}
-            showPrintMargin={true}
-            showGutter={true}
-            highlightActiveLine={true}
-            style={{marginLeft:'150px', width: '80%', height: '650px' }}
+          mode={language === 'cpp' ? 'c_cpp' : 'python'}
+          theme="monokai"
+          value={code}
+          onChange={handleCodeChange}
+          fontSize={18}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          style={{ marginLeft:'70px', height: '500px', width: '90%' }}
         />
+        <button onClick={handleSubmit}>Compile Code</button>
+        
       </div>
     </div>
   );
